1caaf596a0eb4e36dce2c439a58886613e1c9f6b
GreeWojack <teddy78711@gmail.com>
2024-01-26 15:52:24 +0100

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,10 @@
 import "../style/footer.scss";
+import logo from "../assets/logo-white.png"
 
 function Footer() {
     return (
         <div className="footer">
+            <img src={logo} alt="" />
             hehe
         </div>
     );
